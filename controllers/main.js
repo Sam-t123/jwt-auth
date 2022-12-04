34aaa9fce8cbbbce9dfae6e9d8461078ae877aaf
Samarth Tiwari <tiwarisamarth01@gmail.com>
2022-12-05 00:15:26 +0530

    Added throwing of error when username or password in empty

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,18 @@
+// Check username and password in post(login) request
+// if exist create new jwt
+// send back to front end
+
+const CustomAPIError = require('../errors/custom-error');
+
 const login = async (req, res) => {
-  res.status(200).json({
-    msg: 'You sent this data right?',
-    data: req.body,
-  });
+  const { username, password } = req.body;
+  // mongo
+  // Joi
+  // check in controller
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+  console.log(username, password);
 };
 
 const dashboard = async (req, res) => {
