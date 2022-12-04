fb9cf4b6f0289301460642ef7f818ad6707eff16
Samarth Tiwari <tiwarisamarth01@gmail.com>
2022-12-05 01:24:17 +0530

    Added Authorization logic in middleware instead of dashboard function

--- controllers/main.js
+++ controllers/main.js
@@ -24,24 +24,11 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    throw new CustomAPIError('No token provided', 401);
-  }
-  const token = authHeader.split(' ')[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    // console.log(decoded);
-    const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw CustomAPIError('Unauthorized access', 401);
-  }
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
